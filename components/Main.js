82561ebb45d2b8701e51fa2c9f49f9ae8e6c1073
udaymamidisetti <udaymamidisetti5@gmail.com>
2023-12-06 22:51:01 +0530

    update

--- components/Main.js
+++ components/Main.js
@@ -18,7 +18,7 @@ const Main = () => {
           <h1 className="py-4 text-gray-700">
             Hi, I&#39;m <span className="text-[#5651e5]"> Uday Balram</span>
           </h1>
-          <h1 className="py-2 text-gray-700">
+          {/* <h1 className="py-2 text-gray-700">
             An Experienced Front-End Web Developer
           </h1>
           <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
@@ -28,6 +28,11 @@ const Main = () => {
             and visually appealing interfaces that not only meet design
             specifications but also ensure optimal performance across various
             devices.
+          </p> */}
+          <h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
+          <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
+            I’m focused on building responsive front-end web applications
+            integrating back-end technologies.
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <a
